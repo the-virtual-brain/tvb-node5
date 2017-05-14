c16cba896eb1bc76d4b6db82fb302b4d1f947c7f
Marmaduke Woodman <mmwoodman@gmail.com>
2017-05-14 13:00:56 +0200

    reorg

--- data-prep/run-notebook.sh
+++ data-prep/run-notebook.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-docker run --rm -it \
+# default installation of Docker on Linux requires root privileges
+if [[ "$(uname)" == "Linux" ]]
+then
+    docker_cmd="sudo docker"
+elif [[ "$(uname)" == "Darwin" ]]
+then
+    docker_cmd="docker"
+else
+    echo "Who _are_ you?"
+    exit 1
+fi
+
+echo "invoking Docker with '$docker_cmd'" 
+
+exit 0
+
+$docker_cmd pull maedoc/recon
+
+$docker_cmd run --rm -it \
     -v $(pwd):/opt/work \
+    -v /Users/maedoc/Data/ol/fs/ol:/opt/freesurfer/subjects/ol \
     -p 127.0.0.1:8888:8888 -p 127.0.0.1:8000:8000 \
     maedoc/recon jupyter notebook --allow-root --ip=0.0.0.0
